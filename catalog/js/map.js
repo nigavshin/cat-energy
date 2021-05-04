7a6418786245f685175d4cc3800d8b261f19725a
Nikita Gavshin <gavshin.nikita@yandex.ru>
2021-05-04 10:43:39 +0400

    bugfix: margins,fonts,remove id in class

--- catalog/js/map.js
+++ catalog/js/map.js
@@ -6,29 +6,19 @@ ymaps.ready(function () {
             searchControlProvider: 'yandex#search'
         }),
 
-        // Создаём макет содержимого.
         MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
             '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
         ),
 
         myPlacemarkWithContent = new ymaps.Placemark([59.938635, 30.323118], {
-            hintContent: 'Собственный значок метки с контентом',
-            balloonContent: 'А эта — новогодняя',
+            hintContent: 'cat-energy метка',
             iconContent: '12'
         }, {
-            // Опции.
-            // Необходимо указать данный тип макета.
             iconLayout: 'default#imageWithContent',
-            // Своё изображение иконки метки.
-            iconImageHref: '../img/map-pin.png',
-            // Размеры метки.
+            iconImageHref: 'img/map-pin.png',
             iconImageSize: [113, 106],
-            // Смещение левого верхнего угла иконки относительно
-            // её "ножки" (точки привязки).
             iconImageOffset: [-24, -24],
-            // Смещение слоя с содержимым относительно слоя с картинкой.
             iconContentOffset: [15, 15],
-            // Макет содержимого.
             iconContentLayout: MyIconContentLayout
         });
 
